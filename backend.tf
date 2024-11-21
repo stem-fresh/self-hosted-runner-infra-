864bade9fcaac1acb55240bf2d2e6ec3bee3f893
dineshsai14211 <dineshsai14211@gmail.com>
2024-11-21 10:04:40 +0530

    iac for runner

--- backend.tf
+++ backend.tf
@@ -1,13 +1,3 @@
-resource "google_storage_bucket" "terraform_state_file" {
-  name          = "self-runner-infra-statefile"  # Replace with your bucket name
-  location      = var.region      # Ensure this matches the provider's region
-  storage_class = "STANDARD"
-
-  versioning {
-    enabled = true
-  }
-}
-
 # backend.tf
 terraform {
   backend "gcs" {
